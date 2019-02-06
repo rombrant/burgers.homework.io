c87ea1c0df546d762e9c355840b8102a9b8fb3ae
roman <gordonfreeman2332@gmail.com>
2019-02-06 16:53:01 +0300

    меню мобильное

--- js/main.js
+++ js/main.js
@@ -2,19 +2,35 @@ function openMenu(){
     const openBtn = document.querySelector('.burgers-menu-button');
     const closeBtn = document.querySelector('.burgers-menu-closed');
     const menu = document.querySelector('.hide-menu');
-    let op = 0.2;
+    
 
 
         openBtn.addEventListener('click', function () {
-            console.log("это работает");
-            menu.style.opacity = 0;
-            setTimeout (function foo() {
-                if (op<1) {
-                    op+=0.1;
+            menu.classList.remove('hide-menu');
+            menu.classList.add('visible');
+            menu.style.opacity= 0;
+            let op = 0.1;
+            setTimeout (function foo(){
+                if (op < 1) {
+                    op += 0.1;
                     menu.style.opacity= op;
-                    setTimeout (foo, 100);
+                    setTimeout (foo, 50);
                 }
-            }, 100);
+            }, 50);
         });
+        closeBtn.addEventListener('click', function () {
+            menu.style.opacity= 1;
+            let op = 1;
+            setTimeout (function doo(){
+                if (op > 0) {
+                    op -= 0.1;
+                    menu.style.opacity= op;
+                    setTimeout (doo, 50);
+                }
+            },50)
+            setTimeout (function (){
+                menu.classList.add('hide-menu');
+            }, 1000);
+        })
 }
 openMenu();
