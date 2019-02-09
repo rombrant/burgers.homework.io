d95f88270b8e34371616df896f47864ff7da6c1f
roman <gordonfreeman2332@gmail.com>
2019-02-09 11:27:35 +0300

    слайдер секции команды

--- js/main.js
+++ js/main.js
@@ -33,4 +33,48 @@ function openMenu(){
             }, 1000);
         })
 }
+
+
+const menuItems = document.querySelectorAll('.team-block__content-accordeon__list-item');
+
+
+        for (const Elem of menuItems) {
+            const active = Elem.classList.contains('team-block__content-accordeon__list-item--active');
+            Elem.addEventListener('click', r=> {
+            if (active) {
+                Elem.classList.remove('team-block__content-accordeon__list-item--active');
+                Elem.querySelector('.team-block__content-accordeon__list-item__photo').style.height=0;
+                Elem.querySelector('.team-block__content-accordeon__list-item__description').style.height=0;
+            }
+            });
+        }
+
+
+    for (const item of  menuItems) {
+        item.addEventListener('click', e=> {
+            const curItem = e.currentTarget;
+            const photo = curItem.querySelector('.team-block__content-accordeon__list-item__photo');
+            const photoBlock = photo.firstElementChild;
+            const reqPhotoHeight = photoBlock.getBoundingClientRect().height;
+            photo.style.height = reqPhotoHeight+'px';
+            const content = curItem.querySelector('.team-block__content-accordeon__list-item__description');
+            const textBlock = content.lastElementChild;
+            const reqHeight = textBlock.getBoundingClientRect().height;
+            content.style.height = reqHeight+'px';
+            const active = item.classList.contains('team-block__content-accordeon__list-item--active');
+            console.log(reqHeight);
+            if (active) {
+                item.classList.remove('team-block__content-accordeon__list-item--active');
+                item.querySelector('.team-block__content-accordeon__list-item__photo').style.height=0;
+                item.querySelector('.team-block__content-accordeon__list-item__description').style.height=0;
+            }
+            else {
+                curItem.classList.add('team-block__content-accordeon__list-item--active');
+            }
+        })
+    }
+
+
+
+
 openMenu();
