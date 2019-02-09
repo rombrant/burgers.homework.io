938baa16f2db411717979b833695091734d656de
roman <gordonfreeman2332@gmail.com>
2019-02-09 15:40:50 +0300

    слайдер с командой доработан

--- js/main.js
+++ js/main.js
@@ -38,43 +38,42 @@ function openMenu(){
 const menuItems = document.querySelectorAll('.team-block__content-accordeon__list-item');
 
 
-        for (const Elem of menuItems) {
-            const active = Elem.classList.contains('team-block__content-accordeon__list-item--active');
-            Elem.addEventListener('click', r=> {
-            if (active) {
-                Elem.classList.remove('team-block__content-accordeon__list-item--active');
-                Elem.querySelector('.team-block__content-accordeon__list-item__photo').style.height=0;
-                Elem.querySelector('.team-block__content-accordeon__list-item__description').style.height=0;
-            }
-            });
-        }
-
-
     for (const item of  menuItems) {
         item.addEventListener('click', e=> {
             const curItem = e.currentTarget;
-            const photo = curItem.querySelector('.team-block__content-accordeon__list-item__photo');
-            const photoBlock = photo.firstElementChild;
-            const reqPhotoHeight = photoBlock.getBoundingClientRect().height;
-            photo.style.height = reqPhotoHeight+'px';
-            const content = curItem.querySelector('.team-block__content-accordeon__list-item__description');
-            const textBlock = content.lastElementChild;
-            const reqHeight = textBlock.getBoundingClientRect().height;
-            content.style.height = reqHeight+'px';
-            const active = item.classList.contains('team-block__content-accordeon__list-item--active');
-            console.log(reqHeight);
-            if (active) {
-                item.classList.remove('team-block__content-accordeon__list-item--active');
-                item.querySelector('.team-block__content-accordeon__list-item__photo').style.height=0;
-                item.querySelector('.team-block__content-accordeon__list-item__description').style.height=0;
+            const isClosedItem = curItem.classList.contains("team-block__content-accordeon__list-item--active");
+            if (isClosedItem) {
+                removeActiveClass(menuItems);
             }
             else {
-                curItem.classList.add('team-block__content-accordeon__list-item--active');
+                removeActiveClass(menuItems);
+                openSlider(item);
             }
+           
+            
         })
     }
 
 
+    function openSlider(item) {
+        const photo = item.querySelector('.team-block__content-accordeon__list-item__photo');
+        const photoBlock = photo.firstElementChild;
+        const reqPhotoHeight = photoBlock.getBoundingClientRect().height;
+        photo.style.height = reqPhotoHeight+'px';
+        const content = item.querySelector('.team-block__content-accordeon__list-item__description');
+        const textBlock = content.lastElementChild;
+        const reqHeight = textBlock.getBoundingClientRect().height;
+        content.style.height = reqHeight+'px';
+        item.classList.add('team-block__content-accordeon__list-item--active');
+    }
+  
+   function removeActiveClass(menuItems) {
+    Array.from(menuItems).forEach(elem => {
+        elem.classList.remove("team-block__content-accordeon__list-item--active");
+        elem.querySelector('.team-block__content-accordeon__list-item__photo').style.height=0;
+        elem.querySelector('.team-block__content-accordeon__list-item__description').style.height=0;
+    });
+   }
 
 
 openMenu();
