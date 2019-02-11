88698aa68d0f9d19a01907196853e27ea1ea6923
roman <gordonfreeman2332@gmail.com>
2019-02-11 20:38:46 +0300

    неверная верстка модальных окон отзывов

--- js/main.js
+++ js/main.js
@@ -1,82 +1,91 @@
-function openMenu(){
-    const openBtn = document.querySelector('.burgers-menu-button');
-    const closeBtn = document.querySelector('.burgers-menu-closed');
-    const menu = document.querySelector('.hide-menu');
-    
+        //МОБИЛЬНОЕ МЕНЮ, МЕНЮ ГАМБУРГЕР, ГЛАВНОЕ МЕНЮ
 
 
-        openBtn.addEventListener('click', function () {
-            menu.classList.remove('hide-menu');
-            menu.classList.add('visible');
-            menu.style.opacity= 0;
-            let op = 0.1;
-            setTimeout (function foo(){
-                if (op < 1) {
-                    op += 0.1;
-                    menu.style.opacity= op;
-                    setTimeout (foo, 50);
-                }
-            }, 50);
-        });
-        closeBtn.addEventListener('click', function () {
-            menu.style.opacity= 1;
-            let op = 1;
-            setTimeout (function doo(){
-                if (op > 0) {
-                    op -= 0.1;
-                    menu.style.opacity= op;
-                    setTimeout (doo, 50);
-                }
-            },50)
-            setTimeout (function (){
-                menu.classList.add('hide-menu');
-            }, 1000);
-        })
-}
-
-
-const menuItems = document.querySelectorAll('.team-block__content-accordeon__list-item');
-
-
-    for (const item of  menuItems) {
-        item.addEventListener('click', e=> {
-            const curItem = e.currentTarget;
-            const isClosedItem = curItem.classList.contains("team-block__content-accordeon__list-item--active");
-            if (isClosedItem) {
-                removeActiveClass(menuItems);
-            }
-            else {
-                removeActiveClass(menuItems);
-                openSlider(item);
-            }
-           
+        function openMenu(){
+            const openBtn = document.querySelector('.burgers-menu-button');
+            const closeBtn = document.querySelector('.burgers-menu-closed');
+            const menu = document.querySelector('.hide-menu');
             
-        })
-    }
 
 
-    function openSlider(item) {
-        const photo = item.querySelector('.team-block__content-accordeon__list-item__photo');
-        const photoBlock = photo.firstElementChild;
-        const reqPhotoHeight = photoBlock.getBoundingClientRect().height;
-        photo.style.height = reqPhotoHeight+'px';
-        const content = item.querySelector('.team-block__content-accordeon__list-item__description');
-        const textBlock = content.lastElementChild;
-        const reqHeight = textBlock.getBoundingClientRect().height;
-        content.style.height = reqHeight+'px';
-        item.classList.add('team-block__content-accordeon__list-item--active');
-    }
-  
-   function removeActiveClass(menuItems) {
-    Array.from(menuItems).forEach(elem => {
-        elem.classList.remove("team-block__content-accordeon__list-item--active");
-        elem.querySelector('.team-block__content-accordeon__list-item__photo').style.height=0;
-        elem.querySelector('.team-block__content-accordeon__list-item__description').style.height=0;
-    });
-   }
+                openBtn.addEventListener('click', function () {
+                    menu.classList.remove('hide-menu');
+                    menu.classList.add('visible');
+                    menu.style.opacity= 0;
+                    let op = 0.1;
+                    setTimeout (function foo(){
+                        if (op < 1) {
+                            op += 0.1;
+                            menu.style.opacity= op;
+                            setTimeout (foo, 50);
+                        }
+                    }, 50);
+                });
+                closeBtn.addEventListener('click', function () {
+                    menu.style.opacity= 1;
+                    let op = 1;
+                    setTimeout (function doo(){
+                        if (op > 0) {
+                            op -= 0.1;
+                            menu.style.opacity= op;
+                            setTimeout (doo, 50);
+                        }
+                    },50)
+                    setTimeout (function (){
+                        menu.classList.add('hide-menu');
+                    }, 1000);
+                })
+        }
+        openMenu();
 
 
-   const menuOffers = document.querySelectorAll('.menu-accordeon__list-item');
+        //АККОРДЕОН МЕНЮ КОММАНДЫ
+
+        const menuItems = document.querySelectorAll('.team-block__content-accordeon__list-item');
+
+
+        for (const item of  menuItems) {
+            item.addEventListener('click', e=> {
+                const curItem = e.currentTarget;
+                const isClosedItem = curItem.classList.contains("team-block__content-accordeon__list-item--active");
+                if (isClosedItem) {
+                    removeActiveClass(menuItems);
+                }
+                else {
+                    removeActiveClass(menuItems);
+                    openSlider(item);
+                }
+            
+                
+            })
+        }
+
+
+        function openSlider(item) {
+            const photo = item.querySelector('.team-block__content-accordeon__list-item__photo');
+            const photoBlock = photo.firstElementChild;
+            const reqPhotoHeight = photoBlock.getBoundingClientRect().height;
+            photo.style.height = reqPhotoHeight+'px';
+            const content = item.querySelector('.team-block__content-accordeon__list-item__description');
+            const textBlock = content.lastElementChild;
+            const reqHeight = textBlock.getBoundingClientRect().height;
+            content.style.height = reqHeight+'px';
+            item.classList.add('team-block__content-accordeon__list-item--active');
+        }
+    
+        function removeActiveClass(menuItems) {
+            Array.from(menuItems).forEach(elem => {
+                elem.classList.remove("team-block__content-accordeon__list-item--active");
+                elem.querySelector('.team-block__content-accordeon__list-item__photo').style.height=0;
+                elem.querySelector('.team-block__content-accordeon__list-item__description').style.height=0;
+            });
+        }
+
+
+        // АККОРДЕОН МЕНЮ
+
+
+        const menuOffers = document.querySelectorAll('.menu-accordeon__list-item');
         
         for ( const offer of menuOffers) {
             offer.addEventListener('click', e=>{
@@ -109,7 +118,7 @@ const menuItems = document.querySelectorAll('.team-block__content-accordeon__lis
 
 
 
-
+        //СЛАЙДЕР МЕНЮ БУРГЕРОВ
 
 
         const btnLeft = document.querySelector('.slider__scroll-btn-left');
@@ -157,4 +166,47 @@ const menuItems = document.querySelectorAll('.team-block__content-accordeon__lis
 
 
 
-openMenu();
+
+
+        //МОДАЛЬНЫЕ ОКНА
+
+
+        const modalButtons = document.querySelectorAll('.feedback-container-button');//кнопки модальных окон
+        const template = document.querySelector('#modal-template').innerHTML;//тэмплейт заготовка 
+        const modal = templateCreator();//обЪект и методы возвращенные из функции
+        //заведение обработчика события
+        for (const curBtn of modalButtons){
+            curBtn.addEventListener('click', e=>{
+                const paragraph = e.target.previousElementSibling.innerHTML;//извлечение текста отзыва
+                modal.setContent(paragraph);
+                modal.open();
+            })
+        }
+        //создание модального окна
+        function templateCreator() {
+            const container = document.createElement('div');//блок обертка
+            container.className = 'popup';//класс обертки
+            container.innerHTML = template;//вкладка тэмплейта в блок обертки
+            const contentBlock = container.querySelector('.popup__content');//блок отзыва
+            const closeBtn = container.querySelector('.popup__close');//кнопка закртия
+                closeBtn.addEventListener('click', e=>{//обработчик закрытия
+                    document.querySelector('.wrapper').removeChild(container);
+                })
+            const overlay = container.querySelector('.overlay');//закрытие по клику фона
+                overlay.addEventListener('click', e=>{//обработчик закрытия по фону 
+                    if (e.target === overlay) {//условие закрытия
+                        closeBtn.click();//метод закрытия
+                      }
+                })
+            //возвращение методов
+            return {
+                open() {
+                    document.querySelector('.wrapper').appendChild = container;//метод обЪекта для открытия
+                },
+                setContent(content) {
+                    contentBlock.innerHTML = content;//метод присвыивания текста отзыва
+                }
+            };
+        }
+
+
